e4b712244687b158e7c4760989f8ae76f9a173b8
sofiiaAhafonova <amita051197@gmail.com>
2017-10-07 02:55:25 +0300

    lab 1,2

---
+++ labs/lab2/app.js
@@ -0,0 +1,174 @@
+
+const projects = require("./projects");
+const prompt = require("prompt");
+process.stdin.setEncoding("utf8");
+
+let  get_cmd = "Please, input your command:  ";
+
+
+process.stdin.on("readable", () => {
+   const input = process.stdin.read();
+   if (input !== null) {
+       let command = input.toString().trim();
+       if (command === "create()") {
+           inputProject()
+           .then(proj_new => projects.create(proj_new))
+           .then((proj_new) => console.log(proj_new));
+       }
+       else if (command === "getAll()") {
+           projects.getAll()
+           .then(returnedString => console.log(returnedString), 
+                 error => console.log(error))
+           .then(askQuestion());
+       }
+       else if (command.search(regExpGetById) !== -1) {
+           const number = command.toString().trim().match(regExpNumber);
+           projects.getById(number)
+           .then(returnedString => console.log(returnedString),  
+                 error => console.log(error))
+            .then(askQuestion());
+       }
+       else if (command.search(regExpUpdate) !== -1) {
+           const number = command.toString().trim().match(regExpNumber);
+           projects.getById(number).then(update => inputField(update))
+           .then(result => projects.update(result))
+            .then(id => console.log('Element with id ' + id + ' was updated'),
+                  error => console.log(error))
+            .then(askQuestion());
+       }
+       else if (command.search(regExpRemove) !== -1) {
+           const number = command.toString().trim().match(regExpNumber);
+           projects.remove(number).then(returnedInt => {
+               console.log('Element with id ' + returnedInt + ' was removed');
+           },  error => console.log(error)).then(askQuestion());
+       }
+       else
+            askQuestion();
+   }
+});
+
+process.stdin.on("end", () => {
+   process.stdout.write("end");
+});
+
+const regExpGetById = /[g][e][t][B][y][I][d][(]\d+[)]/g;
+const regExpRemove = /[r][e][m][o][v][e][(]\d+[)]/g;
+const regExpUpdate = /[u][p][d][a][t][e][(]\d+[)]/g;
+const regExpNumber = /\d+/g;
+
+let project = {
+    properties: {
+        name: {
+            description: "Enter a project name",
+			type: "string",
+            pattern: /^[a-zA-Z0-9]+$/,
+            message: "Name must be only letters",
+            required: true
+        },
+        description: {
+            description: "Enter your project description",
+            type: "string",
+            required: true
+        },
+        status: {
+            description: "Enter your project status",
+            type: "string",
+            required: true
+		},
+		team: {
+			description: "Enter your team name",
+			type: "string",
+            pattern: /^[a-zA-Z0-9]+$/,
+            required: true
+		},
+		man_hour: {
+			description: "Enter man-hour value",
+			pattern: /^[0-9]+$/,
+            type: "string",
+            required: true
+		},	
+        rating: {
+            description: "Enter project rating",
+            pattern: /^[1-5][.][0-9]$/,
+            message: "Rating must be from 1.0 to 5.0",
+            required: true
+        },
+        start_date: {
+            description: "Enter your project start date",
+            pattern: /^\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01])$/,
+            message: "Date must be only YYYY-MM-DD",
+            required: true
+		},
+		finish_date: {
+            description: "Enter your project finish date",
+            pattern: /^\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01])$/,
+            message: "Date must be only YYYY-MM-DD",
+            required: true
+		}
+		
+		
+    }
+};
+ 
+function askQuestion() {
+    process.stdout.write(get_cmd);
+}
+function inputProject() {
+    return new Promise((resolve, reject) => {
+        prompt.get(project,
+            (err, result) => {
+                const projectBuf = {
+                    id: projects.makeId(),
+                    name: result.name,
+                    description: result.description,
+                    status: result.status,
+                    team: result.team,
+                    man_hour: result.man_hour,
+                    rating: result.rating,
+                    start_date: result.start_date,
+                    finish_date: result.finish_date
+                }
+                resolve(projectBuf);
+            });
+    });
+}
+
+function inputField(proj){
+    return new Promise((resolve, reject) => {
+        prompt.get(["field", "toChange"],
+        (err, result) => {
+            switch (result.field) {
+                case "name":
+                    proj.name = result.toChange;
+                    resolve(proj);
+                    break;
+                case "description":
+                    proj.description = result.toChange;
+                    resolve(proj);
+                    break;
+                case "team":
+                    proj.team = result.toChange;
+                    resolve(proj);
+                    break;
+                case "man_hour":
+                    proj.man_hour = result.toChange;
+                    resolve(proj);
+                    break;
+                case "start_date":
+                    proj.start_date = result.toChange;
+                    resolve(proj);
+                    break;
+                case "finish_date":
+                    proj.finish_date = result.toChange;
+                    resolve(proj);
+                    break;
+                default:
+                    reject("Error");
+                    break;
+            }
+        });
+    });
+}
+
+
+askQuestion();
---
+++ labs/lab2/projects.js
@@ -0,0 +1,99 @@
+const fs = require("fs-promise");
+
+let proj_arr = [];
+let gen_id = 0;
+fileReader();
+
+function fileWriter() {
+  		fs.writeFile("projects.json",
+        JSON.stringify(proj_arr, null, 4),
+        (err) => {
+            if (err) throw err;
+        });
+}
+ 
+function fileReader() {
+    const promise = fs.readFile("projects.json");
+    promise.then(fileText => {
+        proj_arr = JSON.parse(fileText);
+    }).catch(() => {
+        return;
+    });
+}
+
+function  makeId() {
+	gen_id++;
+    if (proj_arr.lenght === 0) return gen_id;
+    else {
+        proj_arr.forEach((value) => {
+            if (value.id === gen_id) {
+                return makeId();
+            } else return gen_id;
+        });
+    }
+    return gen_id; 
+}
+function create(projectBuf) {
+    return new Promise((resolve, reject) => {
+        proj_arr.push(projectBuf);
+        fileWriter();
+        resolve(projectBuf);
+        });
+}
+
+function getAll() {
+    return new Promise((resolve, reject) => {
+        if (proj_arr.length !== 0) resolve(proj_arr);
+        else reject("Empty");
+    });
+}
+
+function getById(proj_id) {
+    let response = null;
+    proj_arr.forEach((value) => {
+        if (value.id == proj_id)  response = value;
+    });
+    return new Promise((resolve, reject) => {
+        if (proj_id < 0 || response == null) reject("Wrong id");
+        if (proj_arr.lenght <= 0) reject("Array is empty");
+        resolve(response);
+    });
+}
+
+function update(proj_update) {
+	proj_arr.forEach((proj) => {
+        if (proj.id == proj_update.id) {
+            response = proj_arr.indexOf(proj);
+        }
+    });
+    return new Promise((resolve, reject) => {
+		proj_arr.splice(response, 1, proj_update);
+		fileWriter();
+		resolve(proj_update.id);
+	});
+}
+ 
+function remove(proj_id) {
+    let response = null;
+    proj_arr.forEach((proj) => {
+        if (proj.id == proj_id) {
+            response = proj_arr.indexOf(proj);
+        }
+    });
+    return new Promise((resolve, reject) => {
+        if (proj_id < 0 || response == null) reject("Wrong id");
+        if (proj_arr.lenght === 0) reject("Array is empty");
+        proj_arr.splice(response,1);
+        fileWriter();
+        resolve(proj_id);
+    });
+}
+
+module.exports = {
+    create,
+    getAll,
+    getById,
+    update,
+	remove,
+	makeId
+}
