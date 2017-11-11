e297a0355396df0b1a599055caebdf7c1ab21d12
sofiiaAhafonova <amita051197@gmail.com>
2017-11-12 01:04:20 +0200

    some updates from lab4

--- labs/lab5/app.js
+++ labs/lab5/app.js
@@ -5,26 +5,19 @@ app.set("view engine", "ejs");
 const projects = require("./routes/projects");
 const project_form = require("./routes/project_form");
 const search = require("./routes/search");
-
-var bodyParser = require('body-parser');
-var multer  = require('multer');
-const COLLECTION_NAME = 'images';
-const UPLOAD_PATH = 'uploads';
-const upload = multer({ dest: `${UPLOAD_PATH}/` });
-
+var bodyParser = require('body-parser')
+const busboyBodyParser = require('busboy-body-parser');
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(upload.array()); 
-app.use(express.static('public'));
 
-app.get("/", (req, res) => {
-    try {
-          res.render("index",{});
-    }
-    catch(error) {
-        res.send(error.message);   
-    }
-});
+app.use(express.static('public'));
+//for images
+app.use(busboyBodyParser({ limit: '5mb' }));
+//validator
+var expressValidator = require('express-validator');
+app.use(expressValidator());
+
+
 //database
 let mongoose = require('mongoose')
 mongoose.connect(process.env.MONGODB_URI, {
@@ -36,6 +29,15 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
   console.log('Connected to mongodb successfully.');
 })
+
+app.get("/", (req, res) => {
+    try {
+          res.render("index",{});
+    }
+    catch(error) {
+        res.send(error.message);   
+    }
+});
 
 app.use("/projects", projects);
 app.use("/project_form", project_form);
--- labs/lab5/routes/project_form.js
+++ labs/lab5/routes/project_form.js
@@ -1,29 +1,60 @@
 let express = require("express");
 let router = express.Router();
 let storage = require("./../modules/projects");
+const fs = require("fs-promise");
 
 router.get("/", (req, res, next) => {
     storage.getAll()
-        .then(data => res.render("project_form", {}))
+        .then(data => res.render("project_form", {errors:null}))
         .catch(err => res.sendStatus(500));
 });
 
 router.post('/post_enctype.asp', function(req, res){
     if(!req.body) return res.sendStatus(400);
-    let new_proj = {name : req.body.projName,
-    description : req.body.projDescription,
-   status : req.body.projStatus,
-   team :  req.body.teamName,
-   man_hour :  req.body.manhour,
-    rating :  req.body.projRating,
-    start_date :  req.body.startDate,
-   finish_date :  req.body.finishDate,
-   image : req.body.img
+
+    let logo = req.files.logo;
+    let type ="." + logo.mimetype.substring(6);
+    let base64String = logo.data.toString('base64');
+       
+    req.checkBody('projName', 'projName must be specified.').notEmpty(); 
+    req.checkBody('teamName', 'teamName name must be specified.').notEmpty();
+    req.checkBody('startDate', 'startDate name must be specified.').notEmpty();
+
+
+    req.sanitize('projName').escape();
+    req.sanitize('teamName').escape();
+    req.sanitize('projDescription').escape();
+
+    var errors = req.validationErrors();
+    let new_proj = {
+        name : req.body.projName,
+        description : req.body.projDescription,
+        status : req.body.projStatus,
+        team :  req.body.teamName,
+        man_hour :  req.body.manhour,
+        rating :  req.body.projRating,
+        start_date :  req.body.startDate,
+        finish_date :  req.body.finishDate,
+        image : "./images/" + req.body.projName + type
     }
-    storage.create(new_proj)
-        .then(data =>{
-            let path = "/projects/" +  new_proj.id;
-            res.redirect(path);
-        });
+    if(errors) {
+        res.render('project_form', {errors: errors});
+    } else {
+        storage.getAll()
+            .then(arr => arr.findIndex(pr => pr.name == new_proj.name) != -1)
+            .then(ex =>{
+                if (ex)
+                        res.render('project_form', {errors: ["Project with this name exists"]});
+                else
+                    storage.create(new_proj)
+                        .then(() => fs.writeFile("public/images/" + new_proj.name + type,new Buffer(base64String, 'base64')))
+                        .then(() => res.redirect( "/projects/" +  new_proj.id))
+                })
+        
+       
+    }
+
+   
+
  });
 module.exports = router;
